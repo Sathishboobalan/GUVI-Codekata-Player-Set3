0835aad225a2bfa0dfe02faad8aa02e561fab68d
Sathish Boobalan <31285976+Sathishboobalan@users.noreply.github.com>
2019-05-02 13:29:15 +0530

    Create RemoveAllSpacesInString.java

---
+++ RemoveAllSpacesInString.java
@@ -0,0 +1,15 @@
+
+package guvi.player.set3;
+
+import java.util.Scanner;
+
+ class RemoveAllSpacesInString {
+     public static void main(String[] args) {
+         Scanner scan = new Scanner(System.in);
+         String str1 = scan.next();
+         String str2 = scan.next();
+         str1 = str1.replace("\\s", "");
+         str2 = str2.replace("\\s", "");
+         System.out.println(str1+str2);
+     }
+}
